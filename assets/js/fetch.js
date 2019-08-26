ac7684be98db131c3920cda87602e4b194c25ffb
Perez <ernesto.perez@oerlikon.com>
2019-08-26 13:21:56 +0200

    testing fetch 2

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,7 +1,11 @@
-fetch('http://example.com/movies.json')
+fetch('https://ernex24.github.io/eram/assets/js/api.json')
   .then(function(response) {
     return response.json();
   })
   .then(function(myJson) {
     console.log(myJson);
+    document.getElementById('phone').innerHTML = myJson.phone
+    document.getElementById('mainTitle').innerHTML = myJson.mainTitle
   });
+
+  
