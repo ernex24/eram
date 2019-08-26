0ffbce1ec903fd110b6de924f5c06a4bc7db7a0c
Perez <ernesto.perez@oerlikon.com>
2019-08-26 15:02:08 +0200

    fecthing phone title and carousel pictures

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,11 +1,33 @@
-fetch('https://ernex24.github.io/eram/assets/js/api.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
-    document.getElementById('phone').innerHTML = myJson.phone
-    document.getElementById('mainTitle').innerHTML = myJson.mainTitle
-  });
+window.onload = function() {
+ 
+    const localHost = 'http://127.0.0.1:5500/'
+    const cloud = 'https://ernex24.github.io/eram/'
+
+    fetch( cloud + 'assets/js/api.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+     JSON.stringify(myJson);
+      document.getElementById('phone').innerHTML = myJson.phone
+      document.getElementById('mainTitle').innerHTML = myJson.mainTitle
+      document.getElementById('mainVideo').innerHTML = myJson.mainVideo
+
+      document.querySelector('.flexslider-hero-picture1').style.backgroundImage = "url(" + myJson.mainGallery.picture1 + ")"; 
+
+      document.querySelector('.flexslider-hero-picture2').style.backgroundImage = "url("+ myJson.mainGallery.picture2 +")"; 
+
+      document.querySelector('.flexslider-hero-picture3').style.backgroundImage = "url("+ myJson.mainGallery.picture3 +")"; 
+
+      var video = document.querySelector('#mainVideo');
+      video.src = myJson.mainGallery.mainVideo;
+
+        console.log(video)
+    });
+
+
+  };
+
+
 
   
