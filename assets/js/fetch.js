dca5651ae7879b94eef40a7c771a3d181e38a59b
Ernesto <Prez>
2019-08-26 18:56:44 +0200

    Cambiando todo el contenido a dinamico desde la api de wordpress

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -4,28 +4,28 @@ window.onload = function() {
     const cloud = 'https://ernex24.github.io/eram/'
     const wordPress = 'https://ernesto-perez.com/wp-json/wp/v2/posts/1562'
 
-    fetch( localHost + 'assets/js/api.json')
-    /* fetch(wordPress) */
+    // fetch( localHost + 'assets/js/api.json')
+     fetch(wordPress)
     .then(function(response) {
       return response.json();
     })
     .then(function(myJson) {
      JSON.stringify(myJson);
      console.log(myJson.acf)
-      document.getElementById('phone').innerHTML = myJson.phone
-      document.getElementById('mainTitle').innerHTML = myJson.mainTitle
-      document.getElementById('mainVideo').innerHTML = myJson.mainVideo
-
-      document.querySelector('.flexslider-hero-picture1').style.backgroundImage = "url(" + myJson.mainGallery.picture1 + ")"; 
-
-      document.querySelector('.flexslider-hero-picture2').style.backgroundImage = "url("+ myJson.mainGallery.picture2 +")"; 
-
-      document.querySelector('.flexslider-hero-picture3').style.backgroundImage = "url("+ myJson.mainGallery.picture3 +")"; 
-
-      var video = document.querySelector('#mainVideo');
-      video.src = myJson.mainGallery.mainVideo;
-
-        console.log(video)
+     var eram = myJson.acf
+      document.querySelector('#phone').innerHTML = eram.phone;
+      document.querySelector('#address').innerHTML = eram.address;
+      document.querySelector('#maintitle').innerHTML = eram.maintitle;
+      document.querySelector('#mainVideo').innerHTML = eram.mainVideo;
+      document.querySelector('#mainVideo').src = eram.mainVideo.url;
+      document.querySelector('.flexslider-hero-picture1').style.backgroundImage = "url(" + eram.picture1.url + ")"; 
+      document.querySelector('.flexslider-hero-picture2').style.backgroundImage = "url("+ eram.picture2.url +")"; 
+      document.querySelector('.flexslider-hero-picture3').style.backgroundImage = "url("+ eram.picture3.url +")";
+      document.querySelector('#advertising_title').innerHTML = eram.advertising_title;
+      document.querySelector('#advertising_price').innerHTML = eram.advertising_price;
+      document.querySelector('#advertising_description').innerHTML = eram.advertising_description;
+      document.querySelector('#zahra_text').innerHTML = eram.zahra_text;
+      
     });
 
 
